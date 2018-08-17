7f9e9ea1c816401a8976a102386bbea8dc3e1213
Methsaan <methsaan@gmail.com>
2018-08-17 09:41:37 -0400

    update

---
+++ calculator.sh
@@ -0,0 +1,24 @@
+clear
+echo "Enter operator"
+read op
+echo "Enter first number"
+read f
+echo "Enter second number"
+read s
+if [ "$op" == "add" ]
+then
+	let answer=$f+$s
+fi
+if [ "$op" == "sub" ]
+then
+	let answer=$(expr $f - $s)
+fi
+if [ "$op" == "mul" ]
+then
+	let answer=$f*$s
+fi
+if [ "$op" == "div" ]
+then
+	let answer=$f/$s
+fi
+echo "Answer: $answer"
