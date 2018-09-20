2cb8ae4ab52731b6f3038fc931412f29e13ea57c
Methsaan <methsaan@gmail.com>
2018-09-20 19:58:16 -0400

    update

--- read1.sh
+++ read1.sh
@@ -5,4 +5,4 @@ echo "What is your name ?"
 read name
 echo "How old are you $name ?"
 read age
-echo "Oh, $name, you are just a baby at $age years old!!"
+echo "Oh, $name, you are just a baby at $age yearsz old!";
