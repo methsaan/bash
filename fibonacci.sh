9d17905c90b8a16dba160a037f2c3c2cf3082b4a
Methsaan <methsaan@gmail.com>
2018-12-07 06:24:23 -0500

    seeing pattern

--- fibonacci.sh
+++ fibonacci.sh
@@ -6,13 +6,19 @@ set -o errexit
 
 echo -n -e "Enter length: "
 read len
-number=1
-number2=1
+number=3
+number2=2
+number3=1
+number4=1
+number5=0
+number6=1
 for i in `seq 1 $len`
 do
-	number2=$(( number2+number2 ))
+	number6=$(( number6+0 ))
+	number5=$(( number5+number6 ))
+	number4=$(( number4+number5 ))
+	number3=$(( number3+number4 ))
+	number2=$(( number2+number3 ))
 	number=$(( number+number2 ))
-	echo $number2
-	echo $number
-	echo ""
+	printf "%s     " $number
 done
