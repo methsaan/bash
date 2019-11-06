b01136cbe9937d064ec8039f5bd66282aa8c8248
Methsaan <methsaan@gmail.com>
2019-11-06 06:56:23 -0500

    switched order

--- hourToMinute.sh
+++ hourToMinute.sh
@@ -14,7 +14,6 @@ read hours2
 echo -n -e "Enter minutes: "
 read minutes2
 let totMinutes2=$minutes2+$hours2*60
-
 let timeleft=$totMinutes2-$totMinutes1
 echo "Minutes left: $timeleft"
-echo "Minutes left: $(( timeleft%60 )):$(( timeleft/60 ))"
+echo "Time left: $(( timeleft/60 )):$(( timeleft%60 ))"
