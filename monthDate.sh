ad0b520ad67712be265a58717666a3aae419604e
Methsaan <methsaan@gmail.com>
2020-06-10 08:38:06 -0400

    calculate week day from date

---
+++ monthDate.sh
@@ -0,0 +1,14 @@
+echo "Enter year"
+read month
+echo "Enter date"
+read date
+month=date[0]
+day=date[1]
+
+
+#read -p "Please enter location names separated by space: " location_names
+
+#for location in $location_names
+#do
+#echo $location
+#done
